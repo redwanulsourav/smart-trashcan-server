c0cbb42a2d8f093187ce50e3cbe9a361e487f893
Exoalam <nafiul.arpon@gmail.com>
2022-09-08 23:00:00 +0600

    added new refresh system

--- db_interface.py
+++ db_interface.py
@@ -11,18 +11,20 @@ class Dataset:
         # Load dataset.
 
         face_dirs = os.listdir('face-dataset/')
-
+        face_files = open('list.txt', 'r')
+        faces = face_files.read()
+        faces = faces.split(',')
         for single_face in face_dirs:
-            img_path = f'face-dataset/{single_face}/face.jpg'
-            face_name = f'face-dataset/{single_face}/name.txt'
+            img_path = f'face-dataset/{single_face}'
+            #face_name = f'face-dataset/{single_face}/name.txt'
 
             # Extract name
-            f = open(face_name, 'r')
-            contents = f.read()
-            f.close()
+            # f = open(face_name, 'r')
+            # contents = f.read()
+            # f.close()
 
             # Clean up the contents.
-            name = contents.strip()
+            name = faces[int(single_face[:-4])].strip()
 
             img = face_recognition.load_image_file(img_path)
             encoding = face_recognition.face_encodings(img)[0]
@@ -37,6 +39,14 @@ class Dataset:
     def get_face_name_idx(self, idx):
         if idx < len(self.face_names) and idx >= 0:
             return self.face_names[idx]
-        
+
+    def refresh_db(self, img_path, name):
+        img = face_recognition.load_image_file(img_path)
+        encoding = face_recognition.face_encodings(img)[0]
+
+        self.face_encodings.append(encoding)
+        self.face_names.append(name)
+
+
 
     
